5d96fa0d8eaf13b9cd14d62e917db3628396d22e
w-paulina <wojcikpaulina1@2p.pl>
2023-05-29 20:01:34 +0200

    log user info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Zad1';
+export class AppComponent implements OnInit {
+  port = 4200;
+  author = 'Paulina Wójcik'; 
+
+  clientIp!: string;
+  clientDate!: string;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getIPAddress();
+    console.log(`[INFO] Autor: ${this.author} | Port: ${this.port} | Data uruchomienia: ${new Date()}`);
+  }
+
+    
+  private getIPAddress(): void {
+    this.http.get("http://api.ipify.org/?format=json").subscribe((res:any)=>{
+      this.clientIp = res.ip;
+      this.getDateTime(res.ip)
+      
+    });
+  }
+
+  getDateTime(ip: string) {
+    this.http.get(`https://worldtimeapi.org/api/ip/${ip}`).subscribe((res: any) => {
+      this.clientDate = res.datetime;
+    });
+  }
+
 }
